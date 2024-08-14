66dcc84aac9c851f7b85c5d8610e7186c607fbe2
Guillermo Esteves <g@gesteves.com>
2024-08-14 08:16:16 -0600

    Update prompt

--- lightroomalttextplugin.lrplugin/config.lua
+++ lightroomalttextplugin.lrplugin/config.lua
@@ -1,8 +1,11 @@
 return {
   SYSTEM_PROMPT = [[
 You will receive an image and you will write a short alt text that describes its contents.
-Keep the description factual and objective. Omit subjective details such as the mood of the image, etc.
-You must return  your response in JSON format, using the following structure:
+Keep the description factual and objective. Omit subjective details such as the mood of the image.
+Do not specify if the image is in color or black and white.
+If any text needs to be quoted, use double quotes.
+
+You must return your response in JSON format, using the following structure:
 
 {
   "altText": "A short description of the image."
