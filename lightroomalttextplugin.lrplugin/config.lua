ed273f497e710cff9d7404612f13767e3afda9f6
Guillermo Esteves <g@gesteves.com>
2024-08-17 08:58:38 -0600

    Update prompt.

--- lightroomalttextplugin.lrplugin/config.lua
+++ lightroomalttextplugin.lrplugin/config.lua
@@ -3,12 +3,17 @@ return {
 You will receive an image and you will write a short alt text that describes its contents.
 Keep the description factual and objective. Omit subjective details such as the mood of the image.
 Do not specify if the image is in color or black and white.
-If any text needs to be quoted, use double quotes.
+When quoting text present within the image, you **must** use double quotes (" ").
 
 You must return your response in JSON format, using the following structure:
 
 {
   "altText": "A short description of the image."
+}
+
+Example:
+{
+  "altText": "A sign that says \"Welcome to the park.\""
 }
 ]]
 }
